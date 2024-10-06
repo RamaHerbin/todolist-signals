9b48926348c5b7c8fe502d8d18fb41a3dcc3490c
Rama Herbin <rama.herbin@ansys.com>
2024-10-06 23:53:13 +0200

    base page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatTabsModule, NavbarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'todolist-signals';
